2a12c5451e1b9d1e8975483eba2379ac3f92cd99
Marcelo Alejandro Quispe <112019566+MarceloAlejandroQuispe@users.noreply.github.com>
2023-07-18 12:35:32 -0300

    EJERCICIO con JAVA POO

---
+++ Plantas/src/plantas/Plantas.java
@@ -0,0 +1,16 @@
+package plantas;
+
+public class Plantas {
+
+       public static void main(String[] args) {
+       
+           Arbol arbolito = new Arbol();
+           Flor  florcita = new Flor();
+           Arbusto arbustito = new Arbusto();
+           
+           arbolito.decirLoQueSoy();
+           florcita.decirLoQueSoy();
+           arbustito.decirLoQueSoy();
+    }
+    
+}
